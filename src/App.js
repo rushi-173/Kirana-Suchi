7542dd888a4022adf0b9cd446ae73d164beee7f8
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2021-01-03 23:02:32 +0530

    feat: store and display current value

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  let [currentValue, setCurrentValue] = useState("");
+  function changeHandler(e) {
+    setCurrentValue(e.target.value);
+    console.log(currentValue);
+  }
   return (
     <div className="App">
       <h1>Kirana Suchi</h1>
       <div className="main">
         <div className="input-wrapper">
-          <input />
+          <input type="text" value={currentValue} onChange={changeHandler} />
           <button>+</button>
         </div>
       </div>

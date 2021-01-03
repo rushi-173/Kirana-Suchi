f82e4d88354a005162922ede8b74a508d937c527
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2021-01-03 23:35:39 +0530

    feat: delete from List

--- src/List.js
+++ src/List.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 
 function List(props) {
+  function deleteFromList(itemKey) {
+    let newList = props.itemList.filter((item) => {
+      return item.key !== itemKey;
+    });
+    props.updateItemList(newList);
+  }
   return (
     <div className="item-list">
       {props.itemList.map((itemObj) => {
         return (
           <div className="item" key={itemObj.key}>
             <h3>{itemObj.item}</h3>
-            <button>x</button>
+            <button onClick={() => deleteFromList(itemObj.key)}>x</button>
           </div>
         );
       })}

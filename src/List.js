919b83dd77e3caf2e45901c433ae18b52ccefb7e
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2021-01-03 23:28:25 +0530

    feat: show items from itemList

--- src/List.js
+++ src/List.js
@@ -3,8 +3,14 @@ import React, { useState } from "react";
 function List(props) {
   return (
     <div className="item-list">
-      <h3>item</h3>
-      <button>x</button>
+      {props.itemList.map((itemObj) => {
+        return (
+          <div className="item" key={itemObj.key}>
+            <h3>{itemObj.item}</h3>
+            <button>x</button>
+          </div>
+        );
+      })}
     </div>
   );
 }
